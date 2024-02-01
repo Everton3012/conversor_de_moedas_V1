4128a9cdc98578ab2cb3b4b29f5d950baee3da79
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-02-01 18:07:59 -0300

    cotação com euro e dolar fixos

--- api.php
+++ api.php
@@ -3,24 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conversor</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
-        <h1>Resultado final</h1>
+        <h1>Conversor de Moedas: Euro e Dolar</h1>
         <p>
             <?php 
 
                 $real = $_POST["real"] ?? 0;
 
-                $cotacao = 4.91;
+                $cotacaoDol = 4.91;
 
-                $dolar =  $real / $cotacao;
+                $cotacaoEur = 5.34;
+
+                $dolar =  $real / $cotacaoDol;
+
+                $euro  =  $real / $cotacaoEur;
 
                 $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-                echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivale a " . numfmt_format_currency($padrao, $dolar, "USD");
+                echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivale a " . numfmt_format_currency($padrao, $dolar, "USD") .  " Dolares" . " e também equivale a " . numfmt_format_currency($padrao, $euro, "EUR") . " Euros";
             ?>
     </p>
     <button onclick="javascript:window.location.href='index.html';">&#x2B05; Voltar</button>
